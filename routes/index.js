05cc261cf9955e9cf2627ff69a99cb25faeca2c1
truluci <yilmazmevlut397@gmail.com>
2024-09-12 20:24:33 +0300

    Refactor CSS styles and add new routes and views

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', (req, res) => {
     res.render('index');
 });
 
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+
+router.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
 module.exports = router;
