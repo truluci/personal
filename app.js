f8ae06763c055fb538f7c446b74abb08b0bbf8c9
truluci <yilmazmevlut397@gmail.com>
2024-10-25 23:44:01 +0300

    Refactor CSS styles and add pictures page

--- app.js
+++ app.js
@@ -2,17 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Set view engine to Pug
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
-// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define routes
 app.use('/', require('./routes/index'));
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
