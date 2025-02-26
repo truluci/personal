4b39e7e2279aee1ff3c9af9882b82c2c13dba77c
truluci <yilmazmevlut397@gmail.com>
2025-02-26 22:34:19 +0300

    Add projects page with dynamic content and refactor styles for improved layout

--- controllers/projects/get.js
+++ controllers/projects/get.js
@@ -1,11 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+
 export default (req, res) => {
-    return res.render('projects/projects', {
-      page: 'projects/projects',
-      title: 'projects',
-      includes: {
-        js: ['page'],
-        css: ['header', 'page', 'general']
-      }
-    });
-  };
+  const __dirname = path.resolve();
+  const projects = JSON.parse(fs.readFileSync(path.join(__dirname, '/public/content/projects.json'), 'utf8'));
+
+  return res.render('projects/projects', {
+    page: 'projects/projects',
+    title: 'projects',
+    projects,
+    includes: {
+      js: ['page'],
+      css: ['header', 'page', 'general']
+    }
+  });
+};
   
